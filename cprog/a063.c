0cbc45db655b1542913e08fb7332fbece59c4334
becomelin <becamelin@gmail.com>
2025-02-11 14:29:51 +0800

    Create a063.c

---
+++ cprog/a063.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int n = 0;
+
+void K(int b)
+{
+    n = n + 1;
+    if (b % 4)
+        K(b + 1);
+}
+
+void G(int m)
+{
+    for (int i = 0; i < m; i = i + 1)
+    {
+        K(i);
+    }
+}
+
+int main()
+{
+    G(100);
+    printf("n 的最終值: %d\n", n);
+    return 0;
+}
